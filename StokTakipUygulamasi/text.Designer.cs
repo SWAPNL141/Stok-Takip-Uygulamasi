06ba1b09f27eeb09b91785df74018e593c0e73d7
swp <180960121+SWAPNL141@users.noreply.github.com>
2026-09-01 23:07:47 +0300

    Add files via upload

---
+++ StokTakipUygulamasi/text.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace StokTakipUygulamasi
+{
+    partial class text
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnfrmAnaSayfa = new System.Windows.Forms.Button();
+            this.btnfrmGiris = new System.Windows.Forms.Button();
+            this.btnfrmKategoriIslemleri = new System.Windows.Forms.Button();
+            this.btnfrmMusteriIslemleri = new System.Windows.Forms.Button();
+            this.btnfrmPersonelIslemleri = new System.Windows.Forms.Button();
+            this.btnfrmStokuAzalanUrunler = new System.Windows.Forms.Button();
+            this.btnfrmTedarikciIslemler = new System.Windows.Forms.Button();
+            this.btnfrmUrunIslemleri = new System.Windows.Forms.Button();
+            this.btnfrmSatisDetaylari = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnfrmAnaSayfa
+            // 
+            this.btnfrmAnaSayfa.Location = new System.Drawing.Point(12, 320);
+            this.btnfrmAnaSayfa.Name = "btnfrmAnaSayfa";
+            this.btnfrmAnaSayfa.Size = new System.Drawing.Size(131, 38);
+            this.btnfrmAnaSayfa.TabIndex = 0;
+            this.btnfrmAnaSayfa.Text = "frmAnaSayfa";
+            this.btnfrmAnaSayfa.UseVisualStyleBackColor = true;
+            this.btnfrmAnaSayfa.Click += new System.EventHandler(this.btnfrmAnaSayfa_Click);
+            // 
+            // btnfrmGiris
+            // 
+            this.btnfrmGiris.Location = new System.Drawing.Point(12, 276);
+            this.btnfrmGiris.Name = "btnfrmGiris";
+            this.btnfrmGiris.Size = new System.Drawing.Size(131, 38);
+            this.btnfrmGiris.TabIndex = 1;
+            this.btnfrmGiris.Text = "frmGiris";
+            this.btnfrmGiris.UseVisualStyleBackColor = true;
+            this.btnfrmGiris.Click += new System.EventHandler(this.btnfrmGiris_Click);
+            // 
+            // btnfrmKategoriIslemleri
+            // 
+            this.btnfrmKategoriIslemleri.Location = new System.Drawing.Point(12, 188);
+            this.btnfrmKategoriIslemleri.Name = "btnfrmKategoriIslemleri";
+            this.btnfrmKategoriIslemleri.Size = new System.Drawing.Size(131, 38);
+            this.btnfrmKategoriIslemleri.TabIndex = 2;
+            this.btnfrmKategoriIslemleri.Text = "frmKategoriIslemleri";
+            this.btnfrmKategoriIslemleri.UseVisualStyleBackColor = true;
+            this.btnfrmKategoriIslemleri.Click += new System.EventHandler(this.btnfrmKategoriIslemleri_Click);
+            // 
+            // btnfrmMusteriIslemleri
+            // 
+            this.btnfrmMusteriIslemleri.Location = new System.Drawing.Point(12, 232);
+            this.btnfrmMusteriIslemleri.Name = "btnfrmMusteriIslemleri";
+            this.btnfrmMusteriIslemleri.Size = new System.Drawing.Size(131, 38);
+            this.btnfrmMusteriIslemleri.TabIndex = 2;
+            this.btnfrmMusteriIslemleri.Text = "frmMusteriIslemleri";
+            this.btnfrmMusteriIslemleri.UseVisualStyleBackColor = true;
+            // 
+            // btnfrmPersonelIslemleri
+            // 
+            this.btnfrmPersonelIslemleri.Location = new System.Drawing.Point(12, 100);
+            this.btnfrmPersonelIslemleri.Name = "btnfrmPersonelIslemleri";
+            this.btnfrmPersonelIslemleri.Size = new System.Drawing.Size(131, 38);
+            this.btnfrmPersonelIslemleri.TabIndex = 2;
+            this.btnfrmPersonelIslemleri.Text = "frmPersonelIslemleri";
+            this.btnfrmPersonelIslemleri.UseVisualStyleBackColor = true;
+            // 
+            // btnfrmStokuAzalanUrunler
+            // 
+            this.btnfrmStokuAzalanUrunler.Location = new System.Drawing.Point(12, 144);
+            this.btnfrmStokuAzalanUrunler.Name = "btnfrmStokuAzalanUrunler";
+            this.btnfrmStokuAzalanUrunler.Size = new System.Drawing.Size(131, 38);
+            this.btnfrmStokuAzalanUrunler.TabIndex = 2;
+            this.btnfrmStokuAzalanUrunler.Text = "frmStokuAzalanUrunler";
+            this.btnfrmStokuAzalanUrunler.UseVisualStyleBackColor = true;
+            // 
+            // btnfrmTedarikciIslemler
+            // 
+            this.btnfrmTedarikciIslemler.Location = new System.Drawing.Point(12, 56);
+            this.btnfrmTedarikciIslemler.Name = "btnfrmTedarikciIslemler";
+            this.btnfrmTedarikciIslemler.Size = new System.Drawing.Size(131, 38);
+            this.btnfrmTedarikciIslemler.TabIndex = 2;
+            this.btnfrmTedarikciIslemler.Text = "frmTedarikciIslemler";
+            this.btnfrmTedarikciIslemler.UseVisualStyleBackColor = true;
+            // 
+            // btnfrmUrunIslemleri
+            // 
+            this.btnfrmUrunIslemleri.Location = new System.Drawing.Point(12, 12);
+            this.btnfrmUrunIslemleri.Name = "btnfrmUrunIslemleri";
+            this.btnfrmUrunIslemleri.Size = new System.Drawing.Size(131, 38);
+            this.btnfrmUrunIslemleri.TabIndex = 2;
+            this.btnfrmUrunIslemleri.Text = "frmUrunIslemleri";
+            this.btnfrmUrunIslemleri.UseVisualStyleBackColor = true;
+            // 
+            // btnfrmSatisDetaylari
+            // 
+            this.btnfrmSatisDetaylari.Location = new System.Drawing.Point(12, 364);
+            this.btnfrmSatisDetaylari.Name = "btnfrmSatisDetaylari";
+            this.btnfrmSatisDetaylari.Size = new System.Drawing.Size(131, 38);
+            this.btnfrmSatisDetaylari.TabIndex = 2;
+            this.btnfrmSatisDetaylari.Text = "frmSatisDetaylari\r\n";
+            this.btnfrmSatisDetaylari.UseVisualStyleBackColor = true;
+            this.btnfrmSatisDetaylari.Click += new System.EventHandler(this.btnfrmSatisDetaylari_Click);
+            // 
+            // text
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(149, 450);
+            this.Controls.Add(this.btnfrmUrunIslemleri);
+            this.Controls.Add(this.btnfrmTedarikciIslemler);
+            this.Controls.Add(this.btnfrmSatisDetaylari);
+            this.Controls.Add(this.btnfrmStokuAzalanUrunler);
+            this.Controls.Add(this.btnfrmPersonelIslemleri);
+            this.Controls.Add(this.btnfrmMusteriIslemleri);
+            this.Controls.Add(this.btnfrmKategoriIslemleri);
+            this.Controls.Add(this.btnfrmGiris);
+            this.Controls.Add(this.btnfrmAnaSayfa);
+            this.Name = "text";
+            this.Text = "text";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnfrmAnaSayfa;
+        private System.Windows.Forms.Button btnfrmGiris;
+        private System.Windows.Forms.Button btnfrmKategoriIslemleri;
+        private System.Windows.Forms.Button btnfrmMusteriIslemleri;
+        private System.Windows.Forms.Button btnfrmPersonelIslemleri;
+        private System.Windows.Forms.Button btnfrmStokuAzalanUrunler;
+        private System.Windows.Forms.Button btnfrmTedarikciIslemler;
+        private System.Windows.Forms.Button btnfrmUrunIslemleri;
+        private System.Windows.Forms.Button btnfrmSatisDetaylari;
+    }
+}
---
+++ StokTakipUygulamasi/text.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace StokTakipUygulamasi
+{
+    public partial class text : Form
+    {
+        public text()
+        {
+            InitializeComponent();
+
+            // Eğer Designer'da bazı Click event'leri bağlı değilse burada garantiye alıyoruz
+            this.btnfrmMusteriIslemleri.Click += btnfrmMusteriIslemleri_Click;
+            this.btnfrmPersonelIslemleri.Click += btnfrmPersonelIslemleri_Click;
+            this.btnfrmStokuAzalanUrunler.Click += btnfrmStokuAzalanUrunler_Click;
+            this.btnfrmTedarikciIslemler.Click += btnfrmTedarikciIslemler_Click;
+            this.btnfrmUrunIslemleri.Click += btnfrmUrunIslemleri_Click;
+        }
+
+        private void btnfrmAnaSayfa_Click(object sender, EventArgs e)
+        {
+            frmAnaSayfa frm = new frmAnaSayfa();
+            frm.Show();
+        }
+
+        private void btnfrmGiris_Click(object sender, EventArgs e)
+        {
+            frmGiris frm2 = new frmGiris();
+            frm2.Show();
+        }
+
+        private void btnfrmKategoriIslemleri_Click(object sender, EventArgs e)
+        {
+            frmKategoriIslemleri frm = new frmKategoriIslemleri();
+            frm.Show();
+        }
+
+        private void btnfrmMusteriIslemleri_Click(object sender, EventArgs e)
+        {
+            frmMusteriIslemleri frm = new frmMusteriIslemleri();
+            frm.Show();
+        }
+
+        private void btnfrmPersonelIslemleri_Click(object sender, EventArgs e)
+        {
+            frmPersonelIslemleri frm = new frmPersonelIslemleri();
+            // Mevcut oturum sahibini mümkünse aktar
+            try { frm.oturumAcanPersonelId = frmGiris.personelId; } catch { }
+            frm.Show();
+        }
+
+        private void btnfrmStokuAzalanUrunler_Click(object sender, EventArgs e)
+        {
+            frmStokuAzalanUrunler frm = new frmStokuAzalanUrunler();
+            frm.Show();
+        }
+
+        private void btnfrmTedarikciIslemler_Click(object sender, EventArgs e)
+        {
+            frmTedarikciIslemler frm = new frmTedarikciIslemler();
+            frm.Show();
+        }
+
+        private void btnfrmUrunIslemleri_Click(object sender, EventArgs e)
+        {
+            frmUrunIslemleri frm = new frmUrunIslemleri();
+            frm.Show();
+        }
+
+        private void btnfrmSatisDetaylari_Click(object sender, EventArgs e)
+        {
+            frmSatisDetaylari frm = new frmSatisDetaylari();
+            frm.Show();
+        }
+    }
+}
